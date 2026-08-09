27f9bcbf4ca3ac122b4399d669f3186e7029d3cb
Qin Shi Huang <155852344+blooperson@users.noreply.github.com>
2026-08-09 18:34:17 -0400

    small changes

--- MazeGame/Assets/Scripts/Player/Health.cs
+++ MazeGame/Assets/Scripts/Player/Health.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class Health : MonoBehaviour
 {
@@ -18,6 +19,9 @@ public class Health : MonoBehaviour
     [SerializeField]
     private BasicMovement movement;
 
+    [SerializeField]
+    private LayerMask groundLayer;
+
     public float respawnTime;
 
     private float startingTime;
@@ -31,6 +35,28 @@ public class Health : MonoBehaviour
         defaultHealth = playerHealth;
 
         startingTime = respawnTime;
+
+
+        // set spwan point properly
+        /* 
+        RaycastHit hit;
+
+        if (Physics.Raycast(respawnPoint.transform.position, Vector3.down, out hit, Mathf.Infinity, groundLayer))
+        {
+            // 4. Get the exact Vector3 impact position in world space
+            Vector3 hitPosition = hit.point;
+
+            respawnPoint.position = new Vector3(respawnPoint.transform.position.x, hit.transform.position.y + 2, respawnPoint.transform.position.z);
+
+            // Example usage: log the position or move an object to it
+            Debug.Log("Raycast hit at position: " + hitPosition);
+        }
+
+        respawn();
+        */
+
+
+
 
     }
 
